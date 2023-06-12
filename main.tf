bfc3a8ba1e1152e5d14e722db41f6f8ecb5c3be4
Raghu K <raghuk.vit@gmail.com>
2023-06-12 08:49:10 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -46,6 +46,11 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
