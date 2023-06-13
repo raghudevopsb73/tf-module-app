e955b5317d8b94dd755fffbe66407f5879f23e24
Raghu K <raghuk.vit@gmail.com>
2023-06-13 08:49:01 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -107,6 +107,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
+      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
