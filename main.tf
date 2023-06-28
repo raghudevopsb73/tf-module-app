bb9b2b465a15b97de0e1b062e250392b054b6e72
Raghu K <raghuk.vit@gmail.com>
2023-06-28 08:37:35 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -29,6 +29,13 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
 
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
@@ -66,6 +73,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
 
   launch_template {
