095755930455837107c619b305e9ae77ca02216b
Raghu K <raghuk.vit@gmail.com>
2023-06-28 08:19:57 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -54,9 +54,11 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
-  desired_capacity = var.desired_capacity
-  max_size         = var.max_size
-  min_size         = var.min_size
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+
 
   launch_template {
     id      = aws_launch_template.main.id
