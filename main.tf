650bd724f753872f6ed800f3cd3bc0ff7575d72a
Raghu K <raghuk.vit@gmail.com>
2023-06-28 08:44:49 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -84,11 +84,11 @@ resource "aws_autoscaling_group" "main" {
 
 
 
-//resource "aws_route53_record" "dns" {
-//  zone_id = "Z055331734ICV430E01P7"
-//  name    = "${var.component}-dev"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.instance.private_ip]
-//}
-//
+resource "aws_route53_record" "dns" {
+  zone_id = "Z055331734ICV430E01P7"
+  name    = "${var.component}-dev"
+  type    = "A"
+  ttl     = 30
+  records = []
+}
+
