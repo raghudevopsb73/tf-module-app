6cdcc63c51a8a6aa7354f826bff2d3542e22ebac
Raghu K <raghuk.vit@gmail.com>
2023-06-29 08:26:34 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -34,6 +34,17 @@ resource "aws_lb_target_group" "main" {
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 5
+    path                = "/health"
+    port                = var.app_port
+    protocol            = "HTTP"
+    timeout             = 4
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener_rule" "static" {
