38b0010427cb32e4ebb47cc0b6e23bafa6a10e7b
Raghu K <raghuk.vit@gmail.com>
2023-07-19 08:17:16 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -117,6 +117,18 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
+resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  name                   = "CPULoadDetect"
+  autoscaling_group_name = aws_autoscaling_group.main.name
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 30.0
+  }
+}
+
 
 
 resource "aws_route53_record" "dns" {
